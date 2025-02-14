73cbdc1d0240b56782fc4e50929fbf82d10ed2ad
Alejandro Cortes <46253800+acortes8@users.noreply.github.com>
2025-02-13 20:00:06 -0600

    Add validations to beverage model & edit css
    Validated for presence of fields and unique titles, removed dark mode stylings

--- app/models/beverage.rb
+++ app/models/beverage.rb
@@ -3,4 +3,10 @@ class Beverage < ApplicationRecord
   has_rich_text :ingredient_section
   has_rich_text :instruction_section
   has_rich_text :description
+
+  validates :title, presence: true, uniqueness: true
+  validates_presence_of :featured_image
+  validates_presence_of :ingredient_section
+  validates_presence_of :instruction_section
+  validates_presence_of :description
 end
