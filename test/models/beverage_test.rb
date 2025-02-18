8d580e1d6057b5cff9333b523e3acec789078eb7
Alejandro Cortes <46253800+acortes8@users.noreply.github.com>
2025-02-17 18:54:19 -0600

    Add a new test case to beverage model tests

--- test/models/beverage_test.rb
+++ test/models/beverage_test.rb
@@ -4,4 +4,13 @@ class BeverageTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "beverage attributes must not be empty" do
+    beverage = Beverage.new
+    assert beverage.invalid?
+    assert beverage.errors[:title].any?
+    assert beverage.errors[:featured_image].any?
+    assert beverage.errors[:ingredient_section].any?
+    assert beverage.errors[:instruction_section].any?
+    assert beverage.errors[:description].any?
+  end
 end
