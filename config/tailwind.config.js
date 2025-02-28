72ca78b8547d692760fff8ac60e7a0a6ae375506
Alejandro Cortes <46253800+acortes8@users.noreply.github.com>
2025-02-27 22:22:11 -0600

    Add custom color names in tailwind config
    Easier to reference these thematic colors by a name than their hex value

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
       fontFamily: {
 //         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'tropical-green': '#28A745',
+        'bright-orange': '#FF5733',
+        'deep-blue': '#007BFF',
+        'soft-beige': '#F5F5F5',
+        'golden-yellow': '#FFC107'
+      }
     },
   },
   plugins: [
