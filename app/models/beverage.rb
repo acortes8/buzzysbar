672503a357f1b6ecf9c9b9de726f73a4b3c76e89
Alejandro Cortes <46253800+acortes8@users.noreply.github.com>
2025-12-06 02:57:11 -0600

    Additional validations and turbo stream added

--- app/models/beverage.rb
+++ app/models/beverage.rb
@@ -1,12 +1,20 @@
 class Beverage < ApplicationRecord
+  after_commit -> { broadcast_refresh_later_to "beverages" }
+
   has_one_attached :featured_image
   has_rich_text :ingredient_section
   has_rich_text :instruction_section
   has_rich_text :description
 
   validates :title, presence: true, uniqueness: true
-  validates_presence_of :featured_image
-  validates_presence_of :ingredient_section
-  validates_presence_of :instruction_section
-  validates_presence_of :description
+  validates :featured_image, :ingredient_section, :instruction_section, :description, presence: true
+
+  def acceptable_image
+    return unless featured_image.attached?
+
+    acceptable_types = [ "image/gif", "image/jpeg", "image/png" ]
+    unless acceptable_types.include?(featured_image.content_type)
+      errors.add(:featured_image, "must be a GIF, JPEG, or PNG image")
+    end
+  end
 end
